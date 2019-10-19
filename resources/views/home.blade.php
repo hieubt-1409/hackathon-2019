b79a055fb5177848877a0942443e301de818a40c
hieubui1195 <hieubui1195@gmail.com>
2019-10-19 10:29:45 +0700

    view scaffold

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,26 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                    
+                </div>
+            @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+            <div class="home">
+                <div class="home__container">
+                    <div class="home__title">
+                        Xin chào: <span>{{ $user->name }}</span>
+                    </div>
+                    <div class="home__balance">
+
+                    </div>
+                    @if ($user->type == 'student')
+                        
+                    @else
+                        teacher                        
                     @endif
-
                     You are logged in!
                 </div>
             </div>
